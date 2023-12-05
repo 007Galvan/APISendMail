4b520817b061232af7ec940638fe2dcb0ed6a458
Carlos Galvan <danielgalvanastorga@gmail.com>
2023-12-05 14:56:48 -0600

    almostReady3

--- index.js
+++ index.js
@@ -11,13 +11,20 @@ app.use(express.json())
 //decodifica la informacion y la convierte en formato json//
 app.use(express.urlencoded({extended:true}));
 
-const corsOptions = {
-    origin: 'https://railway.app/', // Replace with your actual client app's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
-  };
+// const corsOptions = {
+//     origin: 'https://railway.app/', // Replace with your actual client app's URL
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+//   };
   
-app.use(cors(corsOptions));
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific origin(s) you want to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
   
 
 //app.use(cors())
