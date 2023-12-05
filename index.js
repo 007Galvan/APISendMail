1f89e0a33e6ae540e9c6499cc1855f5b1ff0222a
Carlos Galvan <danielgalvanastorga@gmail.com>
2023-12-05 16:53:04 -0600

    ready5

--- index.js
+++ index.js
@@ -11,25 +11,7 @@ app.use(express.json())
 //decodifica la informacion y la convierte en formato json//
 app.use(express.urlencoded({extended:true}));
 
-// const corsOptions = {
-//     origin: 'https://railway.app/', // Replace with your actual client app's URL
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
-//   };
-
-
-  
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://apisendmail-production.up.railway.app'); // Replace '*' with the specific origin(s) you want to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-  
-
-//app.use(cors())
+app.use(cors())
 
 app.use('/public', express.static(`${__dirname}/src`))
 
