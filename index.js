922e2a986b1fd1c29b40dffe065f0a9b110bbee3
Carlos Galvan <danielgalvanastorga@gmail.com>
2023-12-05 16:57:10 -0600

    yacasi

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(cors())
 app.use('/public', express.static(`${__dirname}/src`))
 
 app.use("/api", router)
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+app.listen(port, ()=>{
+    console.log(`corriendo en puerto ${port}`);
 })
